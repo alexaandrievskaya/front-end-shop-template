ab7b2b6dc394fd7d86119f3bb385eb9481175f9d
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2020-10-07 01:20:30 +0300

    UPD: added cart counter.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,45 @@
         timerID = setInterval(gotoNext, interval);
     }
 }(2000));
+
+let cartCounterLabel = document.querySelector('#cart-counter');
+let buttonsContainer = document.querySelector('#content-container');
+let cartCounter = 0;
+let cartPrice = 0;
+
+//const btnClickHandler = () => {
+//    console.log('clicked')
+//}
+//buttons.forEach(item => item.addEventListener('click', btnClickHandler));
+const btnClickHandler = (e) => {
+    let target = e.target;
+
+    if (target.classList.contains('item-actions__cart')){
+        cartCounterLabel.innerHTML = `${++cartCounter}`;
+
+        if (cartCounter === 1) cartCounterLabel.style.display = 'block';
+    }
+    // $599 <sup>25</sup>
+    const mockData = +target.parentElement.previousElementSibling.innerHTML.replace(/^\$(\d+)\s\D+(\d+).*$/u, '$1.$2');
+
+    //('$', '').replace(' <sup>', '.').replace('</sup>', '');
+
+    cartPrice = Math.round((cartPrice + mockData) * 100) / 100;
+
+    let restoreHTML = target.innerHTML;
+
+    target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
+
+    buttonsContainer.removeEventListener('click', btnClickHandler);
+    target.disabled = true;
+
+    setTimeout(() => {
+        target.innerHTML = restoreHTML;
+        buttonsContainer.addEventListener('click', btnClickHandler);
+        target.disabled = false;
+    }, 2000)
+};
+
+buttonsContainer.addEventListener('click', btnClickHandler);
+
+// (/^\$(\d+)\s\D+(\d+).*$/u) replace $1.$2
